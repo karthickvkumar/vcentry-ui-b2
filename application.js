17d3b624189d2af73d6c1e816a1e58a4d5452a3e
kvelraj_concord <kvelraj@concord.net>
2021-06-10 08:29:00 +0530

    calculation

--- application.js
+++ application.js
@@ -1,6 +1,32 @@
-function addition(){
-  var a = 100;
-  var b = 200;
+function addition(a, b){
   var result = a + b;
   console.log(result);
 };
+
+function calculation(){
+  var final_result;
+  var number_1 = prompt("Enter first value");
+  var number_2 = prompt("Enter second value");
+
+  var mode = prompt("Enter the calculation sign");
+
+  if(mode === "+"){
+    final_result = parseInt(number_1) + parseInt(number_2);
+  }
+  else if(mode == "-"){
+    final_result = parseInt(number_1) - parseInt(number_2);
+    
+  }
+  else if(mode == "*"){
+    final_result = parseInt(number_1) * parseInt(number_2);
+    
+  }
+  else if(mode == "/"){
+    final_result = parseInt(number_1) / parseInt(number_2);
+
+  }else{
+    final_result = "Please enter a valid calculation sign"
+  }
+
+  console.log(final_result)
+}
